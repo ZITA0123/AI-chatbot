06d6c9b9886feb565170814128ab4429e7a71196
zita zita <fontsingzita@gmail.com>
2022-07-18 11:04:28 +0100

    AI chatbot for dental assistance

---
+++ train_chatbot.py
@@ -0,0 +1,134 @@
+import nltk #Natural Language ToolKit
+#NTLK: tools for cleaning up text and preparing it for deep learning algorithms
+#nltk.download('all')
+nltk.download('punkt')
+#Punkt Sentence Tokenizer. This tokenizer divides a text into a list of sentences,
+nltk.download('wordnet')
+nltk.download('stopwords')
+#WordNet is a lexical database for the English language, which was created by Princeton, and is part of the NLTK corpus.
+#You can use WordNet alongside the NLTK module to find the meanings of words, synonyms, antonyms, and more. Let's cover some examples.
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+from nltk.corpus import stopwords
+import string
+punctuations=string.punctuation
+
+
+import json #loads json files directly into Python(for simple text serialization)
+import pickle # loads pickle files
+#Pickle (Marshalling/flattening): for serializing/de-serializing any Python object structures(list,tuple,dic,classes,methods...)
+#Serialization:Process of converting a python object into a byte stream to store it in a file/database,transport data over the network or maintain program state across sessions
+#Byte Stream:could represent a text file, or an image, or a live video stream
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+#from keras.optimizers import SGD #doesn't work
+from tensorflow.keras.optimizers import SGD
+import random
+ 
+import os 
+os.chdir('D:/PFE/Agent_conversationnel/chatboot')
+words=[]
+classes = []
+documents = []
+ignore_words = ['?', '!']
+data_file = open('D:/PFE/Agent_conversationnel/chatboot/intents.json').read()
+intents = json.loads(data_file)
+
+
+#stopwords.words('english')
+stop_words = set(stopwords.words('english')) 
+
+for intent in intents['intents']:
+    #if intent['context']==["no text provide"]:
+    if intent['patterns']==[]:
+        classes.append("noanswer")    
+    for pattern in intent['patterns']:
+        print(pattern, type(pattern)," ############")
+       
+        #tokenize each word
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        #add documents in the corpus
+        documents.append((w, intent['tag']))
+        if pattern=="":
+            classes.append("noanswer") 
+                 
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+#remove punctuations
+words=[ w for w in words if w not in punctuations]
+#remove stopwords
+#words=remove_stop_word(words)
+#words=[ w for w in words if w not in stop_words]
+# lemmaztize and lower each word and remove duplicates
+words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
+words = sorted(list(set(words)))
+
+# sort classes
+classes = sorted(list(set(classes)))
+
+# documents = combination between patterns and intents
+print (len(documents), "documents")
+
+# classes = intents
+print (len(classes), "classes", classes)
+
+# words = all words, vocabulary
+print (len(words), "unique lemmatized words", words)
+
+
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
+
+# create our training data
+training = []
+# create an empty array for our output
+output_empty = [0] * len(classes)
+# training set, bag of words for each sentence
+for doc in documents:
+    # initialize our bag of words
+    bag = []
+    # list of tokenized words for the pattern
+    pattern_words = doc[0]
+    # lemmatize each word - create base word, in attempt to represent related words
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
+    # create our bag of words array with 1, if word match found in current pattern
+    for w in words:
+        bag.append(1) if w in pattern_words else bag.append(0)
+    
+    # output is a '0' for each tag and '1' for current tag (for each pattern)
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+    
+    training.append([bag, output_row])
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+ 
+# create train and test lists. X - patterns, Y - intents
+train_x = list(training[:,0])
+train_y = list(training[:,1])
+print("Training data created")
+
+
+# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
+# equal to number of intents to predict output intent with softmax
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#fitting and saving the model 
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+print("model created")
